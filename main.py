99b21bf5afb88cc180173d7cc4f6b2db2141d927
IlyaMashukov <mashukov_ilya@mail.ru>
2025-05-27 14:05:29 +0300

    when_all_break

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import asyncio
+
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from service import create_tables, get_users, add_user
+
+# app = FastAPI()
 
 
-if __name__ == "__main__":
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+# asyncio.run(create_tables())
+asyncio.run(add_user())
+asyncio.run(get_users())
+
+
+# if __name__ == "__main__":
+#     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
--- models.py
+++ models.py
@@ -1,8 +1,52 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Table, ForeignKey, MetaData
+from sqlalchemy.orm import declarative_base, relationship
 
-Base = declarative_base()
 
-class Users(Base):
+metadata = MetaData(schema='marketplace')
+Base = declarative_base(metadata=metadata)
+
+
+class User(Base):
+    """Класс - модель для таблицы user"""
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    email = Column(String)
+    basket = relationship("Product", secondary="user_basket", back_populates="users")
 
 
 class Product(Base):
+    """Класс - модель для таблицы product"""
+    __tablename__ = "product"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    price = Column(Float)
+    amount = Column(Integer)
+    description = Column(String(100))
+    users = relationship("User ", secondary="user_basket", back_populates="basket")
+    shops = relationship("Shop", secondary="shop_products", back_populates="products")
+
+
+class Shop(Base):
+    """Класс - модель для таблицы shops"""
+    __tablename__ = "shop"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    products = relationship("Product", secondary="shop_products", back_populates="shops")
+
+
+"""Определение таблиц-связок без указания схемы"""
+user_basket = Table(
+    'user_basket', Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
+    Column('quantity', Integer, default=1)
+)
+
+
+shop_products = Table(
+    'shop_products', Base.metadata,
+    Column('shop_id', Integer, ForeignKey('shop.id'), primary_key=True),
+    Column('product_id', Integer, ForeignKey('product.id'), primary_key=True),
+    Column('stock', Integer, default=0)
+)
